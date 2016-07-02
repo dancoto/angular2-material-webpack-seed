f6cee86ec8783e54828ae23e2431de2de2c408fc
Daniel Coto <daniel_coto@live.com>
2016-07-01 20:12:39 -0400

    Initial commit

---
+++ src/components/app/app.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'root',
+  template: require('./app.html'),
+  styles: [require('./app.css')]
+})
+export class AppComponent { }
